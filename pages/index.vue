48dbbabd0ec4ac7d1b577f92234de2922dd68ce7
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-20 15:38:39 -0700

    got a code

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,32 @@
 <template>
   <div>
-    <button>Login with Spotify</button>
+    <a
+      href="https://accounts.spotify.com/authorize?client_id=5258eb9bfe354fef94b82e9f53630b35&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000"
+      >Login with Spotify</a
+    >
   </div>
 </template>
 
-<script></script>
+<script>
+import axios from 'axios'
+
+export default {
+  methods: {
+    authorize() {
+      axios
+        .get('https://accounts.spotify.com/authorize', {
+          params: {
+            client_id: '5258eb9bfe354fef94b82e9f53630b35',
+            response_type: 'code',
+            redirect_uri: 'localhost:3000'
+          }
+        })
+        .catch(function(error) {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
 
 <style></style>
