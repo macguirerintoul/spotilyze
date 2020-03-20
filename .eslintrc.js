ba9e3ff5e2b78dfa9f9a5116958f00e5c97bd5fc
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-20 15:05:38 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:prettier/recommended'
+  ],
+  // required to lint *.vue files
+  plugins: ['vue'],
+  // add your custom rules here
+  rules: {}
+}
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <button>Login with Spotify</button>
+  </div>
+</template>
+
+<script></script>
+
+<style></style>
