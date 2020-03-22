d7c07637a0699a201bf20ddf8f90f4c953d90ec5
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-22 15:28:30 -0700

    trying to use straight up vega

--- components/ParallelCoordinatesTracks.vue
+++ components/ParallelCoordinatesTracks.vue
@@ -1,5 +1,15 @@
 <template>
-  <vega-lite :data="values" mark="bar" :encoding="encoding" />
+  <vega-lite
+    :$schema="$schema"
+    :width="width"
+    :height="height"
+    :padding="padding"
+    :config="config"
+    :data="vegaData"
+    :marks="marks"
+    :axes="axes"
+    :scales="scales"
+  />
 </template>
 <script>
 export default {
@@ -9,26 +19,92 @@ export default {
         .$post('.netlify/functions/getAudioFeatures', { ids: result })
         .then(result => {
           console.log(result)
+          this.tracks = result
         })
     })
   },
+  computed: {
+    vegaData() {
+      return [
+        {
+          name: 'tracks',
+          values: this.tracks,
+          async: true,
+          format: { type: 'json', parse: 'auto' }
+        },
+        {
+          name: 'fields',
+          values: [
+            'acousticness',
+            'danceability',
+            'energy',
+            'instrumentalness',
+            'liveness',
+            'speechiness',
+            'valence'
+          ]
+        }
+      ]
+    }
+  },
   data() {
     return {
-      values: [
-        { val: 1 },
-        { val: 5 },
-        { val: 3 },
-        { val: 4 },
-        { val: 5 },
-        { val: 6 },
-        { val: 7 },
-        { val: 8 },
-        { val: 9 }
+      $schema: 'https://vega.github.io/schema/vega/v5.json',
+      width: 800,
+      height: 800,
+      padding: 5,
+      config: {
+        axisY: {
+          titleX: -2,
+          titleY: 410,
+          titleAngle: 0,
+          titleAlign: 'right',
+          titleBaseline: 'top'
+        }
+      },
+      tracks: [],
+      scales: [
+        {
+          name: 'ord',
+          type: 'point',
+          range: 'width',
+          round: true,
+          domain: { data: 'fields', field: 'data' }
+        }
       ],
-      encoding: {
-        x: { bin: true, field: 'val', type: 'quantitative' },
-        y: { aggregate: 'count', type: 'quantitative' }
-      }
+      axes: [
+        {
+          orient: 'left',
+          zindex: 1,
+          scale: 'acousticness',
+          title: 'acousticness',
+          offset: { scale: 'ord', value: 'acousticness', mult: -1 }
+        }
+      ],
+      marks: [
+        {
+          type: 'group',
+          from: { data: 'tracks' },
+          marks: [
+            {
+              type: 'line',
+              from: { data: 'fields' },
+              encode: {
+                enter: {
+                  x: { scale: 'ord', field: 'data' },
+                  y: {
+                    scale: { datum: 'data' },
+                    field: { parent: { datum: 'data' } }
+                  },
+                  stroke: { value: 'steelblue' },
+                  strokeWidth: { value: 1.01 },
+                  strokeOpacity: { value: 0.3 }
+                }
+              }
+            }
+          ]
+        }
+      ]
     }
   }
 }
