53073a463fe554d257ae7da7aec6f11a6f36961e
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-22 15:33:30 -0700

    2 bars

--- components/ParallelCoordinatesTracks.vue
+++ components/ParallelCoordinatesTracks.vue
@@ -1,17 +1,9 @@
 <template>
-  <vega-lite
-    :$schema="$schema"
-    :width="width"
-    :height="height"
-    :padding="padding"
-    :config="config"
-    :data="vegaData"
-    :marks="marks"
-    :axes="axes"
-    :scales="scales"
-  />
+  <div id="viz"></div>
 </template>
 <script>
+import embed from 'vega-embed'
+
 export default {
   created() {
     this.$axios.$get('/.netlify/functions/getAllTracks').then(result => {
@@ -23,7 +15,107 @@ export default {
         })
     })
   },
+  async mounted() {
+    await embed('#viz', this.vegaSpec, { actions: false })
+  },
   computed: {
+    vegaSpec() {
+      return {
+        $schema: 'https://vega.github.io/schema/vega/v5.json',
+        width: 400,
+        height: 200,
+        padding: 5,
+
+        data: [
+          {
+            name: 'table',
+            values: [
+              { category: 'A', amount: 28 },
+              { category: 'B', amount: 55 },
+              { category: 'C', amount: 43 },
+              { category: 'D', amount: 91 },
+              { category: 'E', amount: 81 },
+              { category: 'F', amount: 53 },
+              { category: 'G', amount: 19 },
+              { category: 'H', amount: 87 }
+            ]
+          }
+        ],
+
+        signals: [
+          {
+            name: 'tooltip',
+            value: {},
+            on: [
+              { events: 'rect:mouseover', update: 'datum' },
+              { events: 'rect:mouseout', update: '{}' }
+            ]
+          }
+        ],
+
+        scales: [
+          {
+            name: 'xscale',
+            type: 'band',
+            domain: { data: 'table', field: 'category' },
+            range: 'width',
+            padding: 0.05,
+            round: true
+          },
+          {
+            name: 'yscale',
+            domain: { data: 'table', field: 'amount' },
+            nice: true,
+            range: 'height'
+          }
+        ],
+
+        axes: [
+          { orient: 'bottom', scale: 'xscale' },
+          { orient: 'left', scale: 'yscale' }
+        ],
+
+        marks: [
+          {
+            type: 'rect',
+            from: { data: 'table' },
+            encode: {
+              enter: {
+                x: { scale: 'xscale', field: 'category' },
+                width: { scale: 'xscale', band: 1 },
+                y: { scale: 'yscale', field: 'amount' },
+                y2: { scale: 'yscale', value: 0 }
+              },
+              update: {
+                fill: { value: 'steelblue' }
+              },
+              hover: {
+                fill: { value: 'red' }
+              }
+            }
+          },
+          {
+            type: 'text',
+            encode: {
+              enter: {
+                align: { value: 'center' },
+                baseline: { value: 'bottom' },
+                fill: { value: '#333' }
+              },
+              update: {
+                x: { scale: 'xscale', signal: 'tooltip.category', band: 0.5 },
+                y: { scale: 'yscale', signal: 'tooltip.amount', offset: -2 },
+                text: { signal: 'tooltip.amount' },
+                fillOpacity: [
+                  { test: 'isNaN(tooltip.amount)', value: 0 },
+                  { value: 1 }
+                ]
+              }
+            }
+          }
+        ]
+      }
+    },
     vegaData() {
       return [
         {
