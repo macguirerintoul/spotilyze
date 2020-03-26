d16a68666290c707736a6be721c0570b43ddc661
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-26 14:34:18 -0700

    fix token function???

--- functions/getToken.js
+++ functions/getToken.js
@@ -2,37 +2,42 @@ const axios = require('axios')
 const querystring = require('querystring')
 
 exports.handler = async (event, context) => {
-  const body = querystring.parse(event.body)
-  const code = body.code
-  const redirect_uri = body.redirect_uri
+	const body = querystring.parse(event.body)
+	const code = body.code
+	const redirect_uri = body.redirect_uri
 
-  if (code == null) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ message: 'No code provided' })
-    }
-  }
+	if (code == null) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: 'No code provided' })
+		}
+	} else if (redirect_uri == null) {
+		return {
+			statusCode: 400,
+			body: JSON.stringify({ message: 'No redirect URI provided' })
+		}
+	}
 
-  let clientId = process.env.SPOTIFY_CLIENT_ID
-  let clientSecret = process.env.SPOTIFY_CLIENT_SECRET
-  let b64 = Buffer.from(clientId + ':' + clientSecret).toString('base64')
+	let clientId = process.env.SPOTIFY_CLIENT_ID
+	let clientSecret = process.env.SPOTIFY_CLIENT_SECRET
+	let b64 = Buffer.from(clientId + ':' + clientSecret).toString('base64')
 
-  let params = new URLSearchParams()
-  params.append('grant_type', 'authorization_code')
-  params.append('redirect_uri', redirect_uri)
-  params.append('code', code)
-  return axios
-    .post('https://accounts.spotify.com/api/token', params, {
-      headers: {
-        Authorization: 'Basic ' + b64,
-        'Content-Type': 'application/x-www-form-urlencoded'
-      }
-    })
-    .then(result => {
-      return {
-        statusCode: 200,
-        body: JSON.stringify(result.data)
-      }
-    })
-    .catch(error => console.error(error))
+	let params = new URLSearchParams()
+	params.append('grant_type', 'authorization_code')
+	params.append('redirect_uri', redirect_uri)
+	params.append('code', code)
+	return axios
+		.post('https://accounts.spotify.com/api/token', params, {
+			headers: {
+				Authorization: 'Basic ' + b64,
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+		})
+		.then(result => {
+			return {
+				statusCode: 200,
+				body: JSON.stringify(result.data)
+			}
+		})
+		.catch(error => console.error(error))
 }
--- pages/app.vue
+++ pages/app.vue
@@ -1,40 +1,40 @@
 <template>
-  <div>
-    <h2>Hello, {{ user.display_name }}!</h2>
-    <ParallelCoordinatesTracks />
-  </div>
+	<div>
+		<h2>Hello, {{ user.display_name }}!</h2>
+		<ParallelCoordinatesTracks />
+	</div>
 </template>
 <script>
-// TODO use nuxt builtin axios
-import axios from 'axios'
 import ParallelCoordinatesTracks from '~/components/ParallelCoordinatesTracks'
+
 export default {
-  components: { ParallelCoordinatesTracks },
-  created() {
-    this.checkIfAuthenticated()
-  },
-  data() {
-    return {
-      user: {}
-    }
-  },
-  methods: {
-    initialize() {
-      console.log('Initializing app')
-    },
-    checkIfAuthenticated() {
-      this.$axios
-        .$get('https://api.spotify.com/v1/me')
-        .then(result => {
-          // User is already authenticated
-          this.user = result
-          this.initialize()
-        })
-        .catch(error => {
-          // 401 error because user is not yet authenticated, so send them back to the home screen
-          this.$router.push('/')
-        })
-    }
-  }
+	components: { ParallelCoordinatesTracks },
+	created() {
+		this.checkIfAuthenticated()
+	},
+	data() {
+		return {
+			user: {}
+		}
+	},
+	methods: {
+		initialize() {
+			console.log('Initializing app')
+		},
+		checkIfAuthenticated() {
+			this.$axios
+				.$get('https://api.spotify.com/v1/me')
+				.then(result => {
+					// User is already authenticated
+					this.user = result
+					this.initialize()
+				})
+				.catch(error => {
+					// 401 error because user is not yet authenticated, so send them back to the home screen
+					console.log('user not authenticated, redirecting to home screen')
+					this.$router.push('/')
+				})
+		}
+	}
 }
 </script>
