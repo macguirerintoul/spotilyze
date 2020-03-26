f35768c135438fdbd9ce867f62a0d045cfe9683b
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-03-26 13:53:53 -0700

    prettier config

--- functions/getAllTracks.js
+++ functions/getAllTracks.js
@@ -1,49 +1,49 @@
 const axios = require('axios')
 
 const getAllTracks = async (total, auth) => {
-  let tracks = []
-  let promises = []
-  for (i = 0; i < total; i = i + 50) {
-    promises.push(
-      axios
-        .get('https://api.spotify.com/v1/me/tracks?limit=50&offset=' + i, {
-          headers: {
-            Authorization: auth
-          }
-        })
-        .then(result => {
-          let newTracks = result.data.items.map(item => item.track.id)
-          tracks.push.apply(tracks, newTracks)
-        })
-    )
-  }
-  return Promise.all(promises).then(() => {
-    // console.log('all promises resolved')
-    return tracks
-  })
+	let tracks = []
+	let promises = []
+	for (i = 0; i < total; i = i + 50) {
+		promises.push(
+			axios
+				.get('https://api.spotify.com/v1/me/tracks?limit=50&offset=' + i, {
+					headers: {
+						Authorization: auth,
+					},
+				})
+				.then(result => {
+					let newTracks = result.data.items.map(item => item.track.id)
+					tracks.push.apply(tracks, newTracks)
+				})
+		)
+	}
+	return Promise.all(promises).then(() => {
+		// console.log('all promises resolved')
+		return tracks
+	})
 }
 
 exports.handler = async event => {
-  console.log('getAllTracks')
-  const auth = event.headers.authorization
-  const total = await axios
-    .get('https://api.spotify.com/v1/me/tracks', {
-      // make an initial request to find out the total number of tracks in the library
-      headers: {
-        Authorization: auth
-      }
-    })
-    .then(result => {
-      return result.data.total
-    })
-    .catch(error => console.error(error))
+	console.log('getAllTracks')
+	const auth = event.headers.authorization
+	const total = await axios
+		.get('https://api.spotify.com/v1/me/tracks', {
+			// make an initial request to find out the total number of tracks in the library
+			headers: {
+				Authorization: auth,
+			},
+		})
+		.then(result => {
+			return result.data.total
+		})
+		.catch(error => console.error(error))
 
-  // once we know the total number, retrieve every track
-  return getAllTracks(total, auth).then(result => {
-    // console.log(result)
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result)
-    }
-  })
+	// once we know the total number, retrieve every track
+	return getAllTracks(total, auth).then(result => {
+		// console.log(result)
+		return {
+			statusCode: 200,
+			body: JSON.stringify(result),
+		}
+	})
 }
--- functions/getAudioFeatures.js
+++ functions/getAudioFeatures.js
@@ -1,41 +1,41 @@
 const axios = require('axios')
 
 const getAllAudioFeatures = async (ids, auth) => {
-  console.log('getAllAudioFeatures')
-  let promises = []
-  let tracks = []
-  while (ids.length > 0) {
-    let chunk = ids.splice(0, 100)
-    let params = new URLSearchParams()
-    params.append('ids', chunk)
-    promises.push(
-      axios
-        .get('https://api.spotify.com/v1/audio-features', {
-          params: params,
-          headers: {
-            Authorization: auth
-          }
-        })
-        .then(result => {
-          let newTracks = result.data.audio_features
-          tracks.push.apply(tracks, newTracks)
-        })
-    )
-  }
-  return Promise.all(promises).then(() => {
-    return tracks
-  })
+	console.log('getAllAudioFeatures')
+	let promises = []
+	let tracks = []
+	while (ids.length > 0) {
+		let chunk = ids.splice(0, 100)
+		let params = new URLSearchParams()
+		params.append('ids', chunk)
+		promises.push(
+			axios
+				.get('https://api.spotify.com/v1/audio-features', {
+					params: params,
+					headers: {
+						Authorization: auth,
+					},
+				})
+				.then(result => {
+					let newTracks = result.data.audio_features
+					tracks.push.apply(tracks, newTracks)
+				})
+		)
+	}
+	return Promise.all(promises).then(() => {
+		return tracks
+	})
 }
 
 exports.handler = async event => {
-  console.log('getAudioFeatures')
-  const auth = event.headers.authorization
-  const ids = JSON.parse(event.body).ids
+	console.log('getAudioFeatures')
+	const auth = event.headers.authorization
+	const ids = JSON.parse(event.body).ids
 
-  return getAllAudioFeatures(ids, auth).then(result => {
-    return {
-      statusCode: 200,
-      body: JSON.stringify(result)
-    }
-  })
+	return getAllAudioFeatures(ids, auth).then(result => {
+		return {
+			statusCode: 200,
+			body: JSON.stringify(result),
+		}
+	})
 }
