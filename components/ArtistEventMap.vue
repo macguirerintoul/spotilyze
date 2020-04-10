878564a752ed948975dfbc6b6bc840753a0435b5
Macguire Rintoul <macguire.rintoul@gmail.com>
2020-04-10 15:05:20 -0700

    zoomable map

--- components/ArtistEventMap.vue
+++ components/ArtistEventMap.vue
@@ -26,10 +26,100 @@ export default {
 				height: 500,
 				autosize: 'none',
 
+				signals: [
+					{ name: 'tx', update: 'width / 2' },
+					{ name: 'ty', update: 'height / 2' },
+					{
+						name: 'scale',
+						value: 150,
+						on: [
+							{
+								events: { type: 'wheel', consume: true },
+								update:
+									'clamp(scale * pow(1.0005, -event.deltaY * pow(16, event.deltaMode)), 150, 3000)'
+							}
+						]
+					},
+					{
+						name: 'angles',
+						value: [0, 0],
+						on: [
+							{
+								events: 'mousedown',
+								update: '[rotateX, centerY]'
+							}
+						]
+					},
+					{
+						name: 'cloned',
+						value: null,
+						on: [
+							{
+								events: 'mousedown',
+								update: "copy('projection')"
+							}
+						]
+					},
+					{
+						name: 'start',
+						value: null,
+						on: [
+							{
+								events: 'mousedown',
+								update: 'invert(cloned, xy())'
+							}
+						]
+					},
+					{
+						name: 'drag',
+						value: null,
+						on: [
+							{
+								events: '[mousedown, window:mouseup] > window:mousemove',
+								update: 'invert(cloned, xy())'
+							}
+						]
+					},
+					{
+						name: 'delta',
+						value: null,
+						on: [
+							{
+								events: { signal: 'drag' },
+								update: '[drag[0] - start[0], start[1] - drag[1]]'
+							}
+						]
+					},
+					{
+						name: 'rotateX',
+						value: 0,
+						on: [
+							{
+								events: { signal: 'delta' },
+								update: 'angles[0] + delta[0]'
+							}
+						]
+					},
+					{
+						name: 'centerY',
+						value: 0,
+						on: [
+							{
+								events: { signal: 'delta' },
+								update: 'clamp(angles[1] + delta[1], -60, 60)'
+							}
+						]
+					}
+				],
+
 				projections: [
 					{
 						name: 'projection',
-						type: 'mercator'
+						type: 'mercator',
+						scale: { signal: 'scale' },
+						rotate: [{ signal: 'rotateX' }, 0, 0],
+						center: [0, { signal: 'centerY' }],
+						translate: [{ signal: 'tx' }, { signal: 'ty' }]
 					}
 				],
 
